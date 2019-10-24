0b80c321404cfaf3930e13e116c753bb7a279b08
Angelo Nelson <Angelonelson@Outlook.com>
2019-10-24 14:46:02 -0500

    Fixed "!palu" function

--- Bot.py
+++ Bot.py
@@ -15,28 +15,33 @@ async def on_message(message):
     role = message.guild.get_role(message.author.top_role.id)
     if message.author == client.user:
         return
-    if message.content == "!gayroulette":
+    if message.content == "!rr":
         users = message.guild.members
         num = random.randint(0,len(users))
         color = users[num].color
-        await  message.channel.send("Ahoy! @" + str(users[num]) + " me boy! You are tonight's biggest gay! AGHAHAHAHAHAHA")
         role = message.guild.get_role(users[num].top_role.id)
-        if role == "Admin":
+        if role.permissions.administrator:
             await  message.channel.send("Ahoy! @" + str(users[num]) + "that will have to be a negatory me boy!")
         else:
-            await makeGay(role, color)
-    elif "gay" in message.content:
+            await  message.channel.send("Ahoy! @" + str(users[num]) + " me boy! You are now rainbow! AGHAHAHAHAHAHA")
+            await colorChange(role, color)
+    elif "!rainbow" in message.content:
         color = message.author.color
         if not role.permissions.administrator:
-            await  message.channel.send("Ahoy! " + str(message.author.nick) + " you have the big gay! AGHAHAHAHAHAHAHHAHA")
-            await makeGay(role, color)
+            await  message.channel.send("Ahoy! " + str(message.author.nick) + " you have color! AGHAHAHAHAHAHAHHAHA")
+            await colorChange(role, color)
     if message.content == "!palu":
-        await message.channel.send("Up smash best smash\n:beamend:\n:beam:\n:beam:\n:palu2:\n:palu1:")
+        emoji = discord.utils.get(client.emojis, name = "beamend")
+        await message.channel.send(emoji)
+        for i in range(random.randint(0,10)):
+            await message.channel.send(discord.utils.get(client.emojis, name = "beam"))
+        await message.channel.send(discord.utils.get(client.emojis, name = "palu2"))
+        await message.channel.send(discord.utils.get(client.emojis, name = "palu1"))
     if message.content == "end" and role.permissions.administrator:
         print("Bye")
         exit(0)
 
-async def makeGay(role, color):
+async def colorChange(role, color):
     r = 255
     g = 0
     b = 0
