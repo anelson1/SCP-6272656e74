0ce856f2e6e5988da323ac75c901ad9a10e8f622
Angelo Nelson <angelonelson@outlook.com>
2021-09-22 16:46:36 -0500

    included font and added script to create required folders at startup

--- src/__init__.py
+++ src/__init__.py
@@ -4,6 +4,15 @@ intents = discord.Intents.default()
 intents.members = True
 client = discord.Client(intents = intents)
 authkey=os.getenv("botkey")
+dir = ("src/meme_functionality/images")
+CHECK_FOLDER = os.path.isdir(dir)
+
+# If folder doesn't exist, then create it.
+if not CHECK_FOLDER:
+    os.makedirs(dir)
+    os.makedirs(dir+"/output")
+else:
+    print(dir, "folder already exists.")
 
 from src import functionality
 
