8d0ae2bc61e35ebd021d6871eac027141cced511
Angelo Nelson <angelonelson@outlook.com>
2021-10-23 13:31:46 -0500

    updated bool function to used embeds

--- src/bool_functionality/bool.py
+++ src/bool_functionality/bool.py
@@ -1,6 +1,7 @@
 from src.constants import MAIN_SERVER_ID, GENERAL_ID, DEV_CHAN_ID, BOOL_ROLE_ID, DEV_ROLE_ID
-from src import client
+from src import client, discord
 import json
+from datetime import datetime, time
 
 # Parse message and update JSON file with name and RSVP Status
 
@@ -10,11 +11,16 @@ async def bool_RSVP(message):
         if message.content == "!Y":
             dict = {str(message.author): "Yes"}
             await message.channel.send("You agreed to bool!")
-            await client.get_channel(GENERAL_ID).send(str(message.author) + " has agreed to bool!")
+            embed = discord.Embed(title=message.author, description="Has agreed to bool!", color=discord.Colour.green())
+            embed.set_thumbnail(url=message.author.avatar_url)
+            embed.set_footer(text="Copyright Nelson Net 2021")
+            await client.get_channel(GENERAL_ID).send(embed = embed)
         if message.content == "!N":
             dict = {str(message.author): "No"}
-            await message.channel.send("You turned down the offer to bool.")
-            await client.get_channel(GENERAL_ID).send(str(message.author) + " has turned down the offer to bool!")
+            embed = discord.Embed(title=message.author, description="Has turned down the offer to bool!", color=discord.Colour.red())
+            embed.set_thumbnail(url=message.author.avatar_url)
+            embed.set_footer(text="Copyright Nelson Net 2021")
+            await client.get_channel(GENERAL_ID).send(embed = embed)
         invnum = open("src/bool_functionality/invnum.txt").read()
         try:
             myfile = open("src/bool_functionality/"+invnum+".json", "r+")
@@ -39,23 +45,34 @@ async def bool_RSVP(message):
 
 
 async def bool_send(message):
-    if message.author.id != 213559733758459905:
-        await message.channel.send("For security reasons, only Angelo can initate this command.")
-    else:
-        await message.channel.send("Sending invites...")
-        param = message.content[6::]
-        datefile = open("src/bool_functionality/booldate.txt", "w+")
-        datefile.write(param)
-        datefile.close()
-        role = message.guild.get_role(BOOL_ROLE_ID)
+    current_time = datetime.now().strftime("%H:%M:%S")
+    current_day = datetime.now().strftime("%m/%d/%y")
+    embed = discord.Embed(title="Bool RSVP", description="Asking the fellas to bool", color=message.author.color)
+    embed.add_field(name = "Bool invites", value="Have been sent by " + message.author.nick + " on " +  current_day + " at " + current_time)
+    embed.set_thumbnail(url=message.author.avatar_url)
+    embed.set_footer(text="Copyright Nelson Net 2021 | " + message.guild.name)
+    await message.channel.send(embed = embed)
+    param = message.content[6::]
+    datefile = open("src/bool_functionality/booldate.txt", "w+")
+    datefile.write(param)
+    datefile.close()
+    role = message.guild.get_role(DEV_ROLE_ID)
+    try:
         invitenumber = open("src/bool_functionality/invnum.txt", "r").read()
-        newinvitenumber = open("src/bool_functionality/invnum.txt", "w+")
-        newinvitenumber.write(str(int(invitenumber) + 1))
-        newinvitenumber.close()
-        for member in role.members:
-            chan = await member.create_dm()
-            await chan.send(str(message.author) + " would like to bool on " + str(param) + ". Please respond with !Y for yes or !N for no.")
-            
+    except:
+        invitenumber = open("src/bool_functionality/invnum.txt", "w+").read()
+        invitenumber = 0
+    newinvitenumber = open("src/bool_functionality/invnum.txt", "w+")
+    newinvitenumber.write(str(int(invitenumber) + 1))
+    newinvitenumber.close()
+    for member in role.members:
+        chan = await member.create_dm()
+        embed = discord.Embed(title="Bool RSVP", description="Asking the fellas to bool", color=message.author.color)
+        embed.add_field(name = str(message.author), value = "Would like to bool on " + str(param) + ". Please respond with !Y for yes or !N for no.")
+        embed.set_thumbnail(url=message.author.avatar_url)
+        embed.set_footer(text="Copyright Nelson Net 2021 | " + message.guild.name)
+        await chan.send(embed = embed)
+           
             
 # List people who have RSVPed to bool
 
@@ -65,9 +82,9 @@ async def bool_list(message):
     boolers = open("src/bool_functionality/"+invnum+".json", "r")
     booldict = json.load(boolers)
     date = open("src/bool_functionality/booldate.txt").read()
-    string = "So far: "
+    embed = discord.Embed(title="Bool List", description="The list of people who agreed to bool on " + date, color=message.author.color)
     for k, v in booldict.items():
         if v == "Yes":
-            string += k + ", "
-    string += "have agreed to bool on " + date
-    await message.channel.send(string)
+            embed.add_field(name = k, value=" has agreed")
+    embed.set_footer(text="Copyright Nelson Net 2021 | " + message.guild.name)
+    await message.channel.send(embed = embed)
