90cf950dc5e47ace93a8e8152b3947eeed862f5f
Angelo Nelson <angelonelson@outlook.com>
2022-05-24 19:19:20 -0500

    finish dead checker

--- src/misc_functionalities/dead_package.py
+++ src/misc_functionalities/dead_package.py
@@ -5,18 +5,18 @@ from datetime import datetime
 today = datetime.today()
 async def dead_check(ctx):
     """Returns when each user messaged last then removes a user if they have not messaged in 1 month"""
-    chan = ctx.channel
     users = ctx.guild.members
-    dead_users = []
     new_msg = await ctx.send("Checking the dead...")
-    for i in users:
-        last_msg=await ctx.channel.history(limit=None).get(author__name=i.name)
-        if not (last_msg is None):
-            if (today - last_msg.created_at).days)) > 30:
-                dead_users.append(i)
-
-    print(dead_users)
-    
+    async with ctx.channel.typing():
+        for i in users:
+            last_msg=await ctx.channel.history(limit=None).get(author__name=i.name)
+            if not (last_msg is None):
+                if (today - last_msg.created_at).days > 30:
+                    await i.remove_roles(ctx.guild.get_role(974090144451809341))
+                    embed = discord.Embed(title = i.display_name, description = "will be killed!", color=i.color)
+                    embed.set_thumbnail(url=i.avatar_url)
+                    embed.set_footer(text=ctx.guild.name + " | Nelson Net 2022")
+                    await ctx.channel.send(embed=embed)    
 
 
 
